214aeaabb6fc25be4e458191b147c2e5e14c3edb
Matt-Norton <Matthew.Norton@mygeorgian.ca>
2016-04-21 23:10:46 -0400

    corrections commit
    some corrections

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,20 +4,6 @@
 
     "use strict";
 
-    var header = $('#mainHeader').load("partials/nav.html", function() {
-        var bodyid = $('body').attr('id');
-        switch (bodyid.toString()) {
-            case "index":
-                $('#indexLink').attr("class", "active");
-                break;
-            case "projects":
-                $('#projectsLink').attr("class", "active");
-                break;
-            case "contact":
-                $('#contactLink').attr("class", "active");
-                break;
-        }
-    });
 
     
 
